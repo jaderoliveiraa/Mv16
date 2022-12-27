329cd47b863e102dd864419850b003e46d755a2d
JhonnyMv16 <67910069+JhonnyMv16@users.noreply.github.com>
2022-12-27 08:36:13 -0300

    Update index.js

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -70,7 +70,7 @@ const Login = () => {
 			<CssBaseline />
 			<div className={classes.paper}>
 				<div>
-					<img style={{ margin: "0 auto", height: "80px", width: "100%" }} src="https://uploaddeimagens.com.br/images/004/266/286/original/talicrm_logo.jpg" alt="TaliCRM" />
+					<img style={{ margin: "0 auto", height: "80px", width: "100%" }} src="https://uploaddeimagens.com.br/images/004/272/392/original/zapsimples.png?1671859165" alt="ChatBut" />
 				</div>
 				<Typography component="h1" variant="h5">
 					{i18n.t("login.title")}
